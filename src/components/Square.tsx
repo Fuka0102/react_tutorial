609d48ee3fc20a7f4a509f8a1da02238fc976b16
zumi0102 <tanakafuka0102@gmail.com>
2023-12-31 14:47:55 +0900

    BoardでhandleClickを定義/マスクリック時に実行

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,6 +1,10 @@
 /** @format */
 import { useState } from 'react';
 
-export default function Square({ value }) {
-  return <button className='square'>{value}</button>;
+export default function Square({ value, onSquareClick }) {
+  return (
+    <button className='square' onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
